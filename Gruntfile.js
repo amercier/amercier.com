d7e2bcf8a730fde68166fc28a145ef2586319dbc
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-10-13 18:18:39 +0200

    Added Roboto font, fixed menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -168,7 +168,8 @@ module.exports = function (grunt) {
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
-        relativeAssets: false
+        relativeAssets: false/*,
+        sourcemap: true*/ // grunt-conftrib-sass#master
       },
       dist: {},
       server: {
--- app/scripts/controllers/cv.js
+++ app/scripts/controllers/cv.js
@@ -2,10 +2,4 @@
 
 angular.module('amercierApp')
   .controller('CvCtrl', function ($scope) {
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
