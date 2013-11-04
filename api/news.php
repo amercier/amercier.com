192a305f6938c19232c69514c77538c13aa6152f
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-11-04 19:04:13 +0100

    Started working on API

--- api/news.php
+++ api/news.php
@@ -1,18 +1,25 @@
 <?php
 try {
-  $news = array();
+    $config = include __DIR__ . '/config/config.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 
+    $news = array();
 
+    $consumer = new HTTP_OAuth_Consumer('key', 'secret');
+    $consumer->getRequestToken('https://cloud.feedly.com/v3/auth/token', $callback);
 
-  header('HTTP/1.0 200 OK');
-  header('Content-Type', 'application/json');
-  echo (json_encode($news));
+    $news['token'] = $consumer->getToken();
+    $news['token_secret'] = $consumer->getTokenSecret();
+
+    header('HTTP/1.0 200 OK');
+    header('Content-Type', 'application/json');
+    echo (json_encode($news));
 }
 catch (Exception $e) {
-  header('HTTP/1.0 500 Internal Server Error');
-  header('Content-Type', 'application/json');
-  echo (json_encode(array(
-    'status' => 'failure',
-    'message' => $e->getMessage(),
-  )));
+    header('HTTP/1.0 500 Internal Server Error');
+    header('Content-Type', 'application/json');
+    echo (json_encode(array(
+        'status' => 'failure',
+        'message' => $e->getMessage(),
+    )));
 }
